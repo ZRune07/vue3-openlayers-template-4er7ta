c9b3af4a939242943847d0dbb13a79990ed47b6e
ZRune07 <git@stackblitz.com>
2024-10-15 17:57:02 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import TheMap from './components/TheMap.vue';
+</script>
+
+<template>
+  <TheMap />
+</template>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+
+import OpenLayersMap from 'vue3-openlayers';
+import 'vue3-openlayers/dist/vue3-openlayers.css';
+
+import App from './App.vue';
+
+createApp(App).use(OpenLayersMap).mount('#app');
